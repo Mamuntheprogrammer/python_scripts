21bb9049ca9a9f59ab442f52df2757501a281aee
Mamuntheprogrammer <a.a.mamunbu@gmail.com>
2021-07-05 21:45:30 +0600

    compare data of two different excel sheet

---
+++ compare.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+df1=pd.read_excel('current_month.xlsx')
+df2=pd.read_excel('previous_month.xlsx')
+comparison_values = df1.values == df2.values
+print (comparison_values)
+
+rows,cols=np.where(comparison_values==False)
+
+for item in zip(rows,cols):
+    df1.iloc[item[0], item[1]] = '{} --> {}'.format(df1.iloc[item[0], item[1]],str(df2.iloc[item[0], item[1]])+"-changed")
+
+df1.to_excel('./Excel_diff.xlsx',index=False,header=True)
